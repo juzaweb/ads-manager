fd7f6501ad9b1ccc9dc012999681691a83695e1e
Juzaweb <dangtheanh16@gmail.com>
2023-12-30 16:41:54 +0700

    :+1: Register ads position in themes

--- src/Actions/AdsManagerAction.php
+++ src/Actions/AdsManagerAction.php
@@ -2,20 +2,28 @@
 
 namespace Juzaweb\AdsManager\Actions;
 
+use Juzaweb\AdsManager\Contracts\AdsManager;
 use Juzaweb\AdsManager\Http\Controllers\Frontend\VideoAdsController;
 use Juzaweb\AdsManager\Models\Ads;
 use Juzaweb\AdsManager\Support\Resources\BannerAds;
 use Juzaweb\AdsManager\Support\Resources\VideoAds;
 use Juzaweb\CMS\Abstracts\Action;
+use Juzaweb\CMS\Facades\Theme;
 
 class AdsManagerAction extends Action
 {
+    public function __construct(protected AdsManager $adsManager)
+    {
+        parent::__construct();
+    }
+
     public function handle(): void
     {
         $this->addFilter('posts.get_content', [$this, 'addAdsPost']);
         $this->addAction(Action::BACKEND_INIT, [$this, 'addAdminMenus']);
         $this->addAction(Action::BACKEND_INIT, [$this, 'registerResources']);
         $this->addAction(Action::FRONTEND_INIT, [$this, 'registerFrontendAjaxs']);
+        $this->addAction(Action::INIT_ACTION, [$this, 'registerAdsPositionsInTheme']);
     }
 
     public function addAdminMenus(): void
@@ -64,6 +72,18 @@ class AdsManagerAction extends Action
     public function registerResources(): void
     {
         $this->hookAction->registerResource(BannerAds::class);
-        $this->hookAction->registerResource(VideoAds::class);
+
+        if (in_array('video_ads', Theme::currentTheme()->getRegister('support', []))) {
+            $this->hookAction->registerResource(VideoAds::class);
+        }
+    }
+
+    public function registerAdsPositionsInTheme(): void
+    {
+        $positions = Theme::currentTheme()->getRegister('ads_positions', []);
+
+        foreach ($positions as $key => $position) {
+            $this->adsManager->registerPosition($key, $position['type'] ?? 'banner', $position);
+        }
     }
 }
